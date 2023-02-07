3135c65131f4e2f1af7e66354fe945f051224e82
ajsmaister <ajsmaister@gmail.com>
2023-02-07 23:25:34 +0100

    add postgres_handler.py file

---
+++ utilities/postgres_handler.py
@@ -0,0 +1,47 @@
+
+from sqlalchemy import create_engine
+
+from source_code import sql_conn
+from sql_commands import tables
+
+class PostgresHandler:
+
+	# class variables ...
+	postgres_url = sql_conn # <-- PostgresSQL connection password and username parameters from {source_code.py}
+
+	def __init__(self):
+
+		# instance variables ...
+		self.engine = create_engine(self.postgres_url)
+		self.table = tables['metadata_table']
+		self.create_table() # <-- Definitely, this function is always running when the PostgresHandler will be called!
+
+	def create_table(self):
+
+		# --------------- Add values [New Style formatting] -----------------
+		# create_statement = "...{table_name}..."
+		# self.table = tables['metadata_table'] # <-- from instance variables
+		# create_statement.format(table_name = self.table)
+		# -------------------------------------------------------------------
+
+		create_statement = """
+		select count (*) from information_schema. tables t 
+		where t.table_name = '{table_name}'
+		"""
+		try:
+			with self.engine.connect() as conn:
+				for self.table, cre_script in tables.items():
+
+					temp = conn.execute(create_statement.format(table_name = self.table))
+
+					if temp.fetchone()[0] == 0:
+						conn.execute(cre_script)
+
+						# In Console the Python returns information about creation of DataBase in PostgresSQL ...
+						print("The {table_name} has been created!".format(table_name = str(tables.keys())))
+		except:
+
+			print("The {table_name} already Exist!".format(table_name = tables.keys()))
+
+if __name__ == '__main__':
+	test = PostgresHandler()
